fe62b81c83f7c12846c47f62ba73b01ab13fda51
Kumar Tushar <iamkumartushar@gmail.com>
2023-09-03 12:35:44 +0530

    added api key

--- examples/openai-bot/main.py
+++ examples/openai-bot/main.py
@@ -3,7 +3,7 @@ from textbase.models import OpenAI
 from typing import List
 
 # Load your OpenAI API key
-OpenAI.api_key = ""
+OpenAI.api_key = "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"
 
 # Prompt for GPT-3.5 Turbo
 SYSTEM_PROMPT = """You are chatting with an AI. There are no specific prefixes for responses, so you can ask or talk about anything you like.
